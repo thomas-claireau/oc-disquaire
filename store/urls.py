9b44ae14eb4cc18bc8d2822700dcde9d3dd6cc71
Thomas Claireau <thomas.claireau@gmail.com>
2021-01-24 15:42:18 +0100

    views : add app name

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import include, url
 from . import views
 
+app_name = "store"
+
 urlpatterns = [
-    url(r'^$', views.listing),
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail),
-    url(r'^search/$', views.search)
+    url(r'^$', views.listing, name="listing"),
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name="detail"),
+    url(r'^search/$', views.search, name="search")
 ]
