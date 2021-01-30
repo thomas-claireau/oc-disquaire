faa97926f575d61d870b7dfe0a33c72c69b3f514
Thomas Claireau <thomas.claireau@gmail.com>
2021-01-30 09:17:21 +0100

    update site

--- oc_disquaire/settings.py
+++ oc_disquaire/settings.py
@@ -83,6 +83,7 @@ DATABASES = {
         'PASSWORD': '',
         'HOST': '',
         'PORT': '5432',
+        'ATOMIC_REQUESTS': True
     }
 }
 
--- store/views.py
+++ store/views.py
@@ -8,7 +8,10 @@
     """
 
 from django.shortcuts import render, get_object_or_404
-from store.models import Album
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.db import IntegrityError
+from store.models import Album, Contact, Booking
+from .forms import ContactForm, ParagraphErrorList
 
 
 def index(request):
@@ -35,8 +38,21 @@ def listing(request):
         Returns:
                 HttpResponse: Django's response
         """
-    albums = Album.objects.filter(available=True).order_by('-created_at')[:12]
-    context = {"albums": albums}
+    albums_list = Album.objects.filter(available=True).order_by('-created_at')
+    paginator = Paginator(albums_list, 9)
+    page = request.GET.get("page")
+
+    try:
+        albums = paginator.page(page)
+    except PageNotAnInteger:
+        albums = paginator.page(1)
+    except EmptyPage:
+        albums = paginator.page(paginator.num_pages)
+
+    context = {
+        "albums": albums,
+        "pagination": paginator.num_pages > 1
+    }
     return render(request, "store/listing.html", context)
 
 
@@ -53,10 +69,48 @@ def detail(request, album_id):
     album = get_object_or_404(Album, pk=album_id)
 
     context = {
+        "album_id": album_id,
         "album_title": album.title,
         "thumbnail": album.picture,
         "artists_name": ", ".join([artist.name for artist in album.artists.all()]),
     }
+
+    form = ContactForm()
+
+    if request.method == "POST":
+        form = ContactForm(request.POST, error_class=ParagraphErrorList)
+
+        if form.is_valid():
+            try:
+                name = form.cleaned_data['name']
+                email = form.cleaned_data["email"]
+
+                contact = Contact.objects.filter(email=email)
+
+                if not contact.exists():
+                    contact = Contact.objects.create(
+                        email=email,
+                        name=name
+                    )
+                else:
+                    contact = contact.first()
+
+                album = get_object_or_404(Album, pk=album_id)
+                booking = Booking.objects.create(album=album, contact=contact)
+
+                album.available = False
+                album.save()
+
+                context = {
+                    "album_title": album.title
+                }
+
+                return render(request, "store/merci.html", context)
+            except IntegrityError:
+                form.errors["internal"] = "Une erreur est survenue"
+
+    context["form"] = form
+    context["errors"] = form.errors.items()
 
     return render(request, "store/detail.html", context)
 
@@ -72,11 +126,13 @@ def search(request):
         """
 
     query = request.GET.get('query')
+    albums = []
 
-    albums = Album.objects.filter(title__icontains=query)
+    if query:
+        albums = Album.objects.filter(title__icontains=query)
 
-    if not albums:
-        albums = Album.objects.filter(artists__name__icontains=query)
+        if not albums:
+            albums = Album.objects.filter(artists__name__icontains=query)
 
     context = {"albums": albums}
     return render(request, "store/search.html", context)
